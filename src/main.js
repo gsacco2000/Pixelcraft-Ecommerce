562f57fbcd29f16db0d7461f9de39fcd3154c24a
daisyromaniello <daisy.romaniello@unito.it>
2025-09-20 17:25:02 +0200

    main sistemato

--- src/main.js
+++ src/main.js
@@ -3,14 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-// bootstrap css + js
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./assets/styles/style.css";
 
-// il tuo stile personale (mantiene colori/font/animazioni)
-import './assets/styles/style.css';
-
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount('#app');
+createApp(App).use(store).use(router).mount("#app");
