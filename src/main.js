3b40f3658b1f0818473d863682ba4995c84f48c3
Giulia Sacco <gsacco2000@gmail.com>
2025-09-20 22:57:57 +0200

    modifica hero shop

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import BootstrapVueNext from "bootstrap-vue-next";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/styles/style.css";
+
+import BootstrapVueNext from "bootstrap-vue-next";
 
 createApp(App)
   .use(store)
