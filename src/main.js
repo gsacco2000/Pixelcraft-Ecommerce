f4dd9c56e75e299c36fd15ffa0d86758b4e4bf5a
Giulia Sacco <gsacco2000@gmail.com>
2025-09-20 22:07:26 +0200

    import bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BootstrapVueNext from "bootstrap-vue-next";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/styles/style.css";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(BootstrapVueNext) // aggiungi qui BootstrapVueNext
+  .mount("#app");
