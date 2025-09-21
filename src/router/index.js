26342d01665bdb4d052c74056d001ac6a462da80
JessicaScano <jessica.scano93@gmail.com>
2025-09-21 11:02:00 +0200

    about

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import PortfolioView from "@/views/PortfolioView.vue";
+import AboutView from "@/views/AboutView.vue";
 
 const routes = [
   { path: "/", name: "Home", component: HomeView },
   { path: "/portfolio", name: "Portfolio", component: PortfolioView },
+  { path: "/about", name: "About", component: AboutView },
   {
     path: "/shop",
     name: "Shop",
