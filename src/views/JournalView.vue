a88f75f118f70df235b7f522e5c84ad7c007a2ea
Pietro Salciarini <pietro.salciarini@gmail.com>
2025-09-25 21:00:38 +0200

    mt-1 articoli

--- src/views/JournalView.vue
+++ src/views/JournalView.vue
@@ -41,9 +41,7 @@
       </aside>
     </div>
 
-    <br /><br />
-
-    <h2 class="text-center pb-3">Tutti gli articoli</h2>
+    <h2 class="text-center mt-1 pb-3">Tutti gli articoli</h2>
     <div class="container pb-3">
       <div class="row row-cols-2 row-cols-lg-4 g-3">
         <div v-for="cat in categorie" :key="cat" class="col">
