5844e17d3f76ab8de10a14c9fb2de66ab6c09b98
Pietro Salciarini <pietro.salciarini@gmail.com>
2025-09-26 20:32:25 +0200

    modifica hero journal

--- src/views/JournalView.vue
+++ src/views/JournalView.vue
@@ -1,15 +1,15 @@
 <template>
-  <main class="container">
-    <HeroBanner
-      backgroundImage="/img.journal/journal.hero.jpg"
-      title="Journal"
-      description="
+  <HeroBanner
+    backgroundImage="/img.journal/journal.hero.jpg"
+    title="Journal"
+    description="
       <p>Idee, ispirazioni e approfondimenti: il nostro blog è uno spazio dove condividiamo pensieri, esperienze e visioni sul mondo della comunicazione visiva e oltre.</p>
       <p>Qui raccontiamo storie di progetti, riflessioni sui trend emergenti e consigli pratici per affrontare le sfide creative di oggi.</p>
       <p>Dall’identità di brand alla sostenibilità nel design, dal web alle arti visive, esploriamo temi che ci appassionano e ci guidano nel nostro lavoro quotidiano. Un luogo di confronto e scoperta, pensato per chi vuole restare in movimento insieme a noi.</p>
       "
-    />
+  />
 
+  <main class="container">
     <div class="row g-0 align-items-stretch">
       <section class="col-lg-9 d-flex flex-column">
         <h2 class="m-3 flex-shrink-0">Articoli recenti</h2>
